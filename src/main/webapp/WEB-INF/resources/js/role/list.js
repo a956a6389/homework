7dca165ed6c6eac813250851d2b92215da5834ee
a956a6389 <pmj8449024487241@163.com>
2021-06-02 22:17:43 +0800

    add role and module

--- src/main/webapp/WEB-INF/resources/js/role/list.js
+++ src/main/webapp/WEB-INF/resources/js/role/list.js
@@ -1,10 +1,43 @@
 /**
  * 
  */
-$(function(){
-	$('.setting-module').click(function(){
+$(function() {
+	$('.setting-module').click(function() {
 		$('.module-div').removeClass('show').addClass('hide');
 		var roleId = $(this).attr('data-role-id');
 		$('#role-module' + roleId).removeClass('hide').addClass('show');
+	});
+
+	$('.role-module-submit-but').click(function() {
+		var moduleDiv = $(this).parents('.module-div');
+		var roleId = moduleDiv.attr('data-role-id');
+		var moduleArr = [];
+		moduleDiv.find('input:checked').each(function(idx, ele) {
+			moduleArr.push($(ele).val());
+		});
+		
+		var token = $("meta[name='_csrf']").attr("content");
+	    var header = $("meta[name='_csrf_header']").attr("content");
+	   /* $(document).ajaxSend(function(e, xhr, options) {
+	        xhr.setRequestHeader(header, token);
+	    });*/
+	    
+		if(moduleArr.length > 0){
+			$.ajax({
+				type : "POST",
+				url : context + "ajax/role/addRoleModule",
+				beforeSend: function(xhr){
+					xhr.setRequestHeader(header, token);
+				},
+				data : {
+					'roleId' : roleId,
+					'moduleIds' : moduleArr.toString()
+				}
+			}).done(function(data) {
+				console.log(data);
+			}).fail(function(data) {
+				console.log(data);
+			});
+		}
 	});
 });
